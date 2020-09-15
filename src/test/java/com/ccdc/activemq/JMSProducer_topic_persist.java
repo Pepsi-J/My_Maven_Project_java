7f96b8eaba8f9ff51469c909d809ce5c8ef76d5b
Pepsi-J <59018811+Pepsi-J@users.noreply.github.com>
2020-09-15 16:59:34 +0800

    消息持久化

--- src/test/java/com/ccdc/activemq/JMSProducer_topic_persist.java
+++ src/test/java/com/ccdc/activemq/JMSProducer_topic_persist.java
@@ -1,6 +1,7 @@
 package com.ccdc.activemq;
 
 import javax.jms.Connection;
+import javax.jms.DeliveryMode;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
@@ -10,68 +11,45 @@ import javax.jms.Topic;
 
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-class Sender_Producer_Test {
+class JMSProducer_topic_persist {
 	public static final String USERNAME = ActiveMQConnection.DEFAULT_USER; // 默认的连接用户名
 	public static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD; // 默认的连接密码
 	public static final String BROKERURL = "tcp://192.168.43.69:61616";
-	public static final String QUEUE_NAME = "testQueue";
 	public static final String TOPIC_NAME = "testTopic";
 	public static ActiveMQConnectionFactory factory;
 	public static Connection connection = null;
 	public static Session session;
-	public static Queue queue;
 	public static Topic topic;
 	public static MessageProducer producer;
 
-	public static void start(char c) throws JMSException {
+	@Test
+	void test() throws JMSException {
 		// 1.创建连接工厂factory,传入指定URL
 		factory = new ActiveMQConnectionFactory(USERNAME, PASSWORD, BROKERURL);// 采用默认用户名、密码
 		// 2.创建连接connection
 		connection = factory.createConnection();
-		// 3.启动连接connection
-		connection.start();
-		// 4.创建Session
+		// 3.创建Session
 		session = connection.createSession(false, session.AUTO_ACKNOWLEDGE);
-		// 5.创建destination 消息的目的地（queue/topic）
-		if (c == 'q') {
-			queue = session.createQueue(QUEUE_NAME);
-			// 6.创建队列生产者
-			producer = session.createProducer(queue);
-		} else {
-			topic = session.createTopic(TOPIC_NAME);
-			// 6.创建主题生产者
-			producer = session.createProducer(topic);
+		// 4.创建destination 消息的目的地（topic）
+		topic = session.createTopic(TOPIC_NAME);
+		// 5.创建主题发布者
+		producer = session.createProducer(topic);
+		// 6.设置主题持久化
+		producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+		// 7.启动连接connection
+		connection.start();
+		for (int i = 1; i <= 10; i++) {
+			// 7.创建消息
+			TextMessage message = session.createTextMessage("发布者发布的主题： topicMessage.No" + i);
+			// 8.发送消息
+			producer.send(message);
 		}
-	}
-
-	public static void stop() throws JMSException {
 		// 9.关闭资源
 		producer.close();
 		session.close();
 		connection.close();
 	}
 
-	public static void Send_Message(char c) throws JMSException {
-		for (int i = 1; i <= 10; i++) {
-			// 7.创建消息
-			TextMessage message = session.createTextMessage("生产者发送的消息：" + (c=='q'?"queueMessage.No":"topicMessage.No") + i);
-			// 8.发送消息
-			producer.send(message);
-		}
-	}
-
-	@Test
-	void testProducer() throws JMSException {
-		char c='t';
-		// 创建MQ并启动
-		start(c);
-		// 发送消息
-		Send_Message(c);//queue ? topic
-		// 关闭资源
-		stop();
-
-		System.out.println("消息发布到MQ完成！");
-	}
 }
